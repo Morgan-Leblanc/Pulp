4baadec42856b02aeece28fc8b93dbe8ca5ec2a8
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2020-01-29 17:23:50 +0100

    first commit

---
+++ front/src/component/Homepage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { withRouter } from 'react-router-dom';
+
+
+class Homepage extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Homepage;
